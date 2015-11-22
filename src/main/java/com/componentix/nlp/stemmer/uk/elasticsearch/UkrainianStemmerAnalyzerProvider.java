81cfd8e3db8b14e05ea205229f7d46668341afe2
Alex Wajda <alexander.wajda@gmail.com>
2015-11-22 01:01:45 +0100

    Upgrade to Elasticsearch 1.4 and Lucene 4.10

--- src/main/java/com/componentix/nlp/stemmer/uk/elasticsearch/UkrainianStemmerAnalyzerProvider.java
+++ src/main/java/com/componentix/nlp/stemmer/uk/elasticsearch/UkrainianStemmerAnalyzerProvider.java
@@ -1,12 +1,12 @@
 package com.componentix.nlp.stemmer.uk.elasticsearch;
 
-import org.apache.lucene.analysis.*;
-import org.apache.lucene.analysis.core.*;
-import org.apache.lucene.analysis.standard.*;
-import org.apache.lucene.analysis.miscellaneous.*;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
-import org.apache.lucene.util.Version;
-import org.elasticsearch.ElasticSearchIllegalArgumentException;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
@@ -14,17 +14,16 @@ import org.elasticsearch.index.Index;
 import org.elasticsearch.index.analysis.AbstractIndexAnalyzerProvider;
 import org.elasticsearch.index.settings.IndexSettings;
 
-import java.io.IOException;
 import java.io.Reader;
 
 public class UkrainianStemmerAnalyzerProvider extends AbstractIndexAnalyzerProvider<Analyzer> {
 
-    private final Analyzer analyzer = new StopwordAnalyzerBase(Version.LUCENE_40) {
+    private final Analyzer analyzer = new StopwordAnalyzerBase() {
         @Override
         protected TokenStreamComponents createComponents(final String fieldName, final Reader reader) {
-            final Tokenizer source = new StandardTokenizer(matchVersion, reader);
-            TokenStream result = new LowerCaseFilter(matchVersion, source);
-            result = new StopFilter(matchVersion, result, stopwords);
+            final Tokenizer source = new StandardTokenizer(reader);
+            TokenStream result = new LowerCaseFilter(source);
+            result = new StopFilter(result, stopwords);
 
             // TODO: use stemExclusionSet
             /*
--- src/main/java/com/componentix/nlp/stemmer/uk/elasticsearch/UkrainianStemmerTokenFilterFactory.java
+++ src/main/java/com/componentix/nlp/stemmer/uk/elasticsearch/UkrainianStemmerTokenFilterFactory.java
@@ -1,16 +1,12 @@
 package com.componentix.nlp.stemmer.uk.elasticsearch;
 
+import com.componentix.nlp.stemmer.uk.Stemmer;
 import org.apache.lucene.analysis.TokenStream;
-import org.elasticsearch.ElasticSearchIllegalArgumentException;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 import org.elasticsearch.index.settings.IndexSettings;
-
-import java.io.IOException;
-
-import com.componentix.nlp.stemmer.uk.Stemmer;
 
 public class UkrainianStemmerTokenFilterFactory extends AbstractTokenFilterFactory {
     private final Stemmer stemmer = new Stemmer();
